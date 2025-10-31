70a4fab90d6808bc7dc3ffc607fe7768a71db0b5
yetundekay <ca.yetunde@gmail.com>
2025-10-31 23:04:03 +0100

    Refactor supabase client initialization

--- src/utils/supabase/supabase.ts
+++ src/utils/supabase/supabase.ts
@@ -1,7 +1,4 @@
 import { createClient } from '@supabase/supabase-js';
 import { projectId, publicAnonKey } from './supabase/info';
 
-export const supabase = createClient(
-  projectId,
-  publicAnonKey
-);
+export const supabase = createClient(projectId, publicAnonKey);
